329aa9726e886d381e5020621c8c4b3a9ccf511f
tminnaar <tminnaar94@gmail.com>
2022-09-29 22:06:37 +0100

    comment box feature with char counter and limit indicator

--- commentBox.js
+++ commentBox.js
@@ -1,11 +1,14 @@
-const input = document.getElementById("comment");
-input.addEventListener("input", (event) => {
-  const textLength = input.value.length;
-
-  if (textLength > 140) {
-    input.style.color = "red";
-  }
-
+const commentBox = document.getElementById("comment");
+commentBox.addEventListener("input", (event) => {
+  const textLength = commentBox.value.length;
+  console.log(textLength);
   const charLengthDisplay = document.getElementById("lengthDisplay");
   charLengthDisplay.textContent = `${textLength}/140`;
+  if (textLength > 140) {
+    commentBox.style.color = "red";
+    charLengthDisplay.style.color = "red";
+  } else {
+    commentBox.style.color = "black";
+    charLengthDisplay.style.color = "black";
+  }
 });
