cb7d032f0f3c6d4af425765835724809baedd42b
tminnaar <tminnaar94@gmail.com>
2022-09-29 23:32:08 +0100

    comment history feature

--- commentBox.js
+++ commentBox.js
@@ -1,9 +1,11 @@
 const commentBox = document.getElementById("comment");
+
 commentBox.addEventListener("input", (event) => {
   const textLength = commentBox.value.length;
-  console.log(textLength);
+
   const charLengthDisplay = document.getElementById("lengthDisplay");
   charLengthDisplay.textContent = `${textLength}/140`;
+
   if (textLength > 140) {
     commentBox.style.color = "red";
     charLengthDisplay.style.color = "red";
@@ -12,3 +14,24 @@ commentBox.addEventListener("input", (event) => {
     charLengthDisplay.style.color = "black";
   }
 });
+
+function submitForm(form) {
+  if (form.comment.value.length > 140) {
+    alert("Character limit exceeded!");
+    return false;
+  }
+
+  const commentElement = document.createElement("p");
+  const commentText = document.createTextNode(form.comment.value);
+  commentElement.appendChild(commentText);
+
+  const nameElement = document.createElement("h3");
+  const nameText = document.createTextNode(form.name.value);
+  nameElement.appendChild(nameText);
+
+  const commentContainer = document.createElement("div");
+  commentContainer.appendChild(commentElement);
+  commentContainer.appendChild(nameElement);
+  commentContainer.classList.add("previousComment");
+  document.getElementById("commentHistory").appendChild(commentContainer);
+}
