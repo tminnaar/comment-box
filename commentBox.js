c3fc422f322ba7de09e8ab1e3ea77ee7ead07f01
tminnaar <tminnaar94@gmail.com>
2022-11-24 15:59:34 +0000

    js comments added

--- commentBox.js
+++ commentBox.js
@@ -1,40 +1,47 @@
 const commentBox = document.getElementById("comment");
 
 commentBox.addEventListener("input", (event) => {
-  const textLength = commentBox.value.length;
+	const textLength = commentBox.value.length;
 
-  const charLengthDisplay = document.getElementById("lengthDisplay");
-  charLengthDisplay.textContent = `${textLength}/140`;
+	//string interpolation to display current character count.
+	const charLengthDisplay = document.getElementById("lengthDisplay");
+	charLengthDisplay.textContent = `${textLength}/140`;
 
-  if (textLength > 140) {
-    commentBox.style.color = "red";
-    charLengthDisplay.style.color = "red";
-  } else {
-    commentBox.style.color = "black";
-    charLengthDisplay.style.color = "black";
-  }
+	//styles input box border and text red if limit exceeded.
+	if (textLength > 140) {
+		commentBox.style.color = "red";
+		charLengthDisplay.style.color = "red";
+	} else {
+		commentBox.style.color = "black";
+		charLengthDisplay.style.color = "black";
+	}
 });
 
 function submitForm(form) {
-  if (form.comment.value.length > 140) {
-    alert("Character limit exceeded!");
-    return false;
-  }
+	//if clicked when character limit exceeded, function is not excecuted and alert pops up.
+	if (form.comment.value.length > 140) {
+		alert("Character limit exceeded!");
+		return false;
+	}
 
-  const commentElement = document.createElement("p");
-  const commentText = document.createTextNode(form.comment.value);
-  commentElement.appendChild(commentText);
+	//creates and appends a new <p> element containing the value from the forms text input.
+	const commentElement = document.createElement("p");
+	const commentText = document.createTextNode(form.comment.value);
+	commentElement.appendChild(commentText);
 
-  const nameElement = document.createElement("h3");
-  const nameText = document.createTextNode(form.name.value);
-  nameElement.appendChild(nameText);
+	//creates and appends a new <h3> element containing the value from the forms name input.
+	const nameElement = document.createElement("h3");
+	const nameText = document.createTextNode(form.name.value);
+	nameElement.appendChild(nameText);
 
-  const commentContainer = document.createElement("div");
-  commentContainer.appendChild(commentElement);
-  commentContainer.appendChild(nameElement);
-  commentContainer.classList.add("previousComment");
+	//when another comment is submitted creates and appends a new <div> element containing the value from the previous comment.
+	const commentContainer = document.createElement("div");
+	commentContainer.appendChild(commentElement);
+	commentContainer.appendChild(nameElement);
+	commentContainer.classList.add("previousComment");
 
-  const commentHistoryContainer = document.getElementById("commentHistory");
-  commentHistoryContainer.appendChild(commentContainer);
-  commentHistoryContainer.style.visibility = "visible";
+	//creates and appends a new <div> element containing the submitted comments and sets visibility to visible.
+	const commentHistoryContainer = document.getElementById("commentHistory");
+	commentHistoryContainer.appendChild(commentContainer);
+	commentHistoryContainer.style.visibility = "visible";
 }
